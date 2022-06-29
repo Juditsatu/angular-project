3e1130a1c0344ad007d84b61073956b249a6e7dc
Juditsatu <100708807+Juditsatu@users.noreply.github.com>
2022-06-29 12:55:34 +0200

    fixes

--- src/app/pages/checkout/checkout.component.ts
+++ src/app/pages/checkout/checkout.component.ts
@@ -1,7 +1,12 @@
-import { tap } from 'rxjs/operators';
+import { Product } from './../products/interface/product.interface';
+import { Details } from './../../shared/interface/order.interface';
+import { switchMap, tap } from 'rxjs/operators';
 import { DataService } from './../products/services/data.service';
 import { Component, OnInit } from '@angular/core';
 import { Store } from 'src/app/shared/interface/store.interface';
+import { NgForm } from '@angular/forms';
+import { Order } from 'src/app/shared/interface/order.interface';
+import { ShoppingCartService } from 'src/app/shared/services/shopping-cart-service';
 
 @Component({
   selector: 'app-checkout',
@@ -17,20 +22,36 @@ export class CheckoutComponent implements OnInit {
     city: ""
   }
   isDelivery = false;
-
+  cart: Product[] = [];
   stores: Store[] = []
-  constructor(private dataSvc: DataService) { }
+  constructor(private dataSvc: DataService, private shoppingCartSvc: ShoppingCartService) { }
 
   ngOnInit(): void {
     this.getStores();
+    this.getDataCart();
   }
 
   onPickupOrDelivery(value: boolean): void {
     this.isDelivery = value;
   }
 
-  onSubmit(value:any): void {
-    console.log("Guardar", value)
+  onSubmit({ value: formData }: NgForm): void {
+    console.log("Guardar", formData);
+    const data: Order = {
+      ...formData,
+      date: this.getCurrentDay(),
+      pickup: this.isDelivery
+    }
+    this.dataSvc.saveOrder(data)
+    .pipe(
+      tap( res => console.log('Order ->', res)),
+      switchMap( (order) => {
+        const details = this.prepareDetails();
+        return this.dataSvc.saveDetailsOrder(details);
+      }),
+      tap( res => console.log('Finish ->', res)),
+    )
+    .subscribe()
   }
 
   private getStores(): void {
@@ -38,5 +59,26 @@ export class CheckoutComponent implements OnInit {
       .pipe(
         tap((stores: Store[]) => this.stores = stores))
       .subscribe()
+  }
+
+  private getCurrentDay(): string {
+    return new Date().toLocaleDateString();
+  }
+
+  private prepareDetails(): Details[]{
+    const details : Details[] = [];
+    this.cart.forEach( res => {
+      console.log(res);
+    })
+    return details;
+  }
+
+  private getDataCart(): void {
+    this.shoppingCartSvc.cartAction$
+    .pipe(
+      tap((products: Product[]) => this.cart = products)
+    )
+    .subscribe()
+
   }
 }
